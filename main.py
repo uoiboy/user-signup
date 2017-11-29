2b14f8fd48ac12d89a1bc1673cf8f952fe268869
doug cook <uoiboy@yahoo.com>
2017-11-28 18:10:55 -0600

    did things

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, redirect, render_template
 import os
-import jinja2
 import cgi
+import jinja2
 
-template.dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env= Jinja2.Environment(loader =jinja.FileSystemLoader(template_dir), autoescape =True)
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env= jinja2.Environment(loader =jinja2.FileSystemLoader(template_dir), autoescape =True)
 
 app= Flask(__name__)
 app.config['DEBUG'] = True
@@ -28,7 +28,7 @@ def display_form():
         if username == " " or len(username) >20 or len(username)<3: 
             user_error = "No spaces and between 3 and 20 characters." 
                                                            
-        if  password == ' ' or len(password)>20 or len(password)<3:
+        elif  password == ' ' or len(password)>20 or len(password)<3:
              pass_error = "No spaces and between 3 and 20 characters"
             
                                                             
@@ -37,19 +37,19 @@ def display_form():
              match_error = "Passwords must match."
         
                                                            
-         email_optional_error =""                                             
-         if "." not in email_optional  and "@" not in email_optional or len(email_optional)>20 or len(email_optional)<3 :
-             email_optional_error = " Needs valid email and no spaces and between 3 and 20 spaces"
+        email_optional_error =""                                             
+        elif "." not in email_optional  and "@" not in email_optional or len(email_optional) > 20 or len(email_optional)<3 :
+            email_optional_error = " Needs valid email and no spaces and between 3 and 20 spaces"
 
-         if user_error or pass_error or match_error or email_optional_error:
-             return render_template('index.html',user_error=user_error, pass_error=pass_error, 
-             match_error=match_error, email_optional_error=email_optional_error)
+        elif user_error or pass_error or match_error or email_optional_error:
+            return render_template('index.html',user_error=user_error, pass_error=pass_error, 
+            match_error=match_error, email_optional_error=email_optional_error)
 
-        if user_error:
+        elif user_error:
             return render_template('index.html', user_error = user_error)
-        
-        if not user_error and not pass_error and not mail_error and not email_optional_error:
-            render_template('welcome.html', username=username) 
+
+        elif not user_error and not pass_error and not email_optional_error:
+            return render_template('welcome.html', username=username) 
 
 
 app.run()
